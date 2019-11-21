cdcde8c80927143bf33ebd4c491e50e641ee7ad0
Rori Ash <aash@lcs.on.ca>
2019-11-21 10:25:42 -0500

    Initial Commit

---
+++ SymbolismIndianHorse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SymbolismIndianHorse
+//
+//  Created by Ash, Aurora on 2019-11-21.
+//  Copyright © 2019 Rori Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
